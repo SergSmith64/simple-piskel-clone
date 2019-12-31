cbc17664f7beacd5924ffa6e5f715dfced05bea6
SergSmith64 <sergsmith2008@yandex.ru>
2019-12-31 20:01:48 +0400

    fix: output path in README

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # simple-piskel-clone
 
-DEMO: https://sergsmith64.github.io/simple-piskel-clone/src/screens/canvas/index.html
+DEMO: https://sergsmith64.github.io/simple-piskel-clone/dist/index.html
--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ import { testSum } from './index';
 
 
 // import "./src/screens/canvas/index.scss";
-import "../src/index.scss";
+import '../src/index.scss';
+
 
 let testSumConsole = testSum();
 console.log("__ЭТО_ТЕСТОВАЯ_СУММА___" + testSumConsole);
