beea922ef9855f2dfed15f39c4c1418c370c1b89
SergSmith64 <sergsmith2008@yandex.ru>
2020-01-06 15:06:19 +0400

    fix: main tool

--- src/app.js
+++ src/app.js
@@ -1,15 +1,15 @@
-// console.log("Empty_File_APP.JS__");
+console.log("ВХОДНОЙ_ФАЙЛ_APP.JS______");
 
 import './screens/canvas/images/favicon.ico';
 
 // import * from './screens/canvas/index';
 import { testSum } from './screens/canvas/index';
 const summa = require('./screens/canvas/index.js');
-// console.log('_SUMMA______'+ summa);
+console.log('_SUMMA______'+ summa);
 
 // import "./src/screens/canvas/index.scss";
 import '../src/screens/canvas/index.scss';
 
 
-// let testSumConsole = testSum();
-// console.log("__ЭТО_ТЕСТОВАЯ_СУММА___" + testSumConsole);
+let testSumConsole = testSum();
+console.log("__ЭТО_ТЕСТОВАЯ_СУММА___" + testSumConsole);
--- src/screens/canvas/index.js
+++ src/screens/canvas/index.js
@@ -1,10 +1,10 @@
-// function testSum (x, y) {
-//   return 999;
-// }
+function testSum (x, y) {
+  return 999;
+}
 
-// module.exports = {
-//   testSum
-// };
+module.exports = {
+  testSum
+};
 
 
 // __ инициализация __
@@ -14,6 +14,8 @@ var ctx = canvas.getContext('2d');
 // ____ делаю дефолтный цвет =ЧЕРНЫЙ=
 var newColor = 'black';
 console.log("дефолтный ЦВЕТ___", newColor);
+
+
 
 // __ ДОБАВЛЯЮ ВЫБОР ЦВЕТА____newColor <-- ввожу цвет из INPUT в HTML__
 document.getElementById('colorPrim').oninput = function() {
@@ -28,7 +30,6 @@ canvas.onmousedown =  function(event) {
   canvas.onmousemove = function(event) {
     var x = event.offsetX;
     var y = event.offsetY;
-    // ctx.fillRect(x-5, y-5, 10,10);
     ctx.fillRect(x-5, y-5, 10,10);
   }
   // ___отменяем рисование при отжатии клавиши мыши
