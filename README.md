404e8ef32eacf330cc58b61d0931e0d424ece062
SergSmith64 <sergsmith2008@yandex.ru>
2020-01-08 16:23:46 +0400

    feat: add canvas background

--- README.md
+++ README.md
@@ -12,4 +12,5 @@ Dec 23 | Jan 4 | 24h | create Webpack assembly
 Dec 30 | Dec 31 | 4h | base structure of the project
 Jan 3 | Jan 6 | 5h | layout of the project
 Jan 4 | Jan 4 | 2h | fix: Webpack assembly
-Jan 4 | Jan 5 | 7h | create main tools
+Jan 4 | Jan 6 | 8h | create main tools
+Jan 7 | Jan 7 | 1h | install jest
--- src/screens/canvas/index.js
+++ src/screens/canvas/index.js
@@ -20,10 +20,10 @@ console.log("дефолтный ЦВЕТ___", newColor);
 // __ ДОБАВЛЯЮ ВЫБОР ЦВЕТА____newColor <-- ввожу цвет из INPUT в HTML__
 document.getElementById('colorPrim').oninput = function() {
   newColor =  this.value;
-
   ctx.fillStyle = newColor;
   console.log("НОВЫЙ ЦВЕТ___", newColor);
 }
+
 
 // __________ Рисую Карандашом ___________
 canvas.onmousedown =  function(event) {
@@ -33,7 +33,23 @@ canvas.onmousedown =  function(event) {
     ctx.fillRect(x-5, y-5, 10,10);
   }
   // ___отменяем рисование при отжатии клавиши мыши
+
   canvas.onmouseup = function() {
     canvas.onmousemove = null;
   }
 };
+
+
+
+
+// ___  Рисую линию_________
+
+//____ предварительно задаю цвет
+// ctx.strokeStyle = "red";
+// ___ предварительно задаю толщину
+// ctx.lineWidth = "8";
+
+// ctx.moveTo(100, 50);
+// ctx.lineTo(400, 500);
+// ctx.lineTo(100, 200);
+// ctx.stroke();
